c2c72c7a5f68471495bcd734d38b67d82eaeb7a7
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2019-01-26 23:36:21 +0300

    all-failures-dialog.sh

---
+++ all-failures-dialog.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+nhroot=$(dirname $0)
+nix-shell -p dialog --command \
+          "${nhroot}/all-failures.sh 2>/dev/null | \
+           xargs echo | \
+           { read xs; \
+             if test -n \"\$xs\"; \
+             then tmpf=\$(mktemp); \
+                  while { dialog --no-items --menu 'Logs of failed derivations:' 0 0 0 \$xs 2>\$tmpf; \
+                          if test \${PIPESTATUS[0]} = 0; then cat \$tmpf | xargs nix log | less --clear-screen; else return 1; fi; }; \
+                  do true; done; \
+             else echo \"Utena:  Let's have tea and laugh together ten years from now, okay? Promise?\"; \
+             fi; }"
